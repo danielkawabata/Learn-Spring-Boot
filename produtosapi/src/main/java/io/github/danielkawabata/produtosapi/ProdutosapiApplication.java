6c346b36c2f8c730f6a67078c0c55d832d2eaa0b
danielkawabata <daniel.kawabata@biotronica.tech>
2025-03-06 15:11:15 -0300

    feat(produtosapi): initial commit, added spring boot procut example project

---
+++ produtosapi/src/main/java/io/github/danielkawabata/produtosapi/ProdutosapiApplication.java
@@ -0,0 +1,14 @@
+package io.github.danielkawabata.produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosapiApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdutosapiApplication.class, args);
+    }
+
+}
