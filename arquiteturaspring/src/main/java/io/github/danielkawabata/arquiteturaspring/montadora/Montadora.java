7d25ca1bbd8694a8b0bea50dc832907a1dff2260
danielkawabata <daniel.kawabata@biotronica.tech>
2025-03-06 19:02:03 -0300

    feat(arquiteturaspring/montadora): added car related raw files into montadora directory

---
+++ arquiteturaspring/src/main/java/io/github/danielkawabata/arquiteturaspring/montadora/Montadora.java
@@ -0,0 +1,6 @@
+package io.github.danielkawabata.arquiteturaspring.montadora;
+
+public enum Montadora {
+    HONDA,
+    TOYOTA
+}
---
+++ arquiteturaspring/src/main/java/io/github/danielkawabata/arquiteturaspring/montadora/TipoMotor.java
@@ -0,0 +1,7 @@
+package io.github.danielkawabata.arquiteturaspring.montadora;
+
+public enum TipoMotor {
+    ASPIRADO,
+    TURBO,
+    ELETRICO
+}
